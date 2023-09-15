417785d59c82cc8f08bf08d9004b2a378b8c5a44
muss001998 <mustapha001hamiai@gmail.com>
2023-09-14 18:40:15 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,7 +10,8 @@ if (n < 0)
 {
 n1 = -n;
 _putchar('-');
-} else
+}
+else
 {
 n1 = n;
 }
@@ -19,25 +20,4 @@ if (n1 / 10)
 print_number(n1 / 10);
 }
 _putchar((n1 % 10) + '0');
-}
-101-main.c
-#include "main.h"
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-print_number(98);
-_putchar('\n');
-print_number(402);
-_putchar('\n');
-print_number(1024);
-_putchar('\n');
-print_number(0);
-_putchar('\n');
-print_number(-98);
-_putchar('\n');
-return (0);
 }
