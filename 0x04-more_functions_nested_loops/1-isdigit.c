6b7d80d29c12b76cc2cef8b034c332681f87531b
muss001998 <mustapha001hamiai@gmail.com>
2023-09-14 17:31:05 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,25 +7,9 @@
 */
 int _isdigit(int c)
 {
-if (c >= '0' && c <= '9')
-return (1);
-else
-return (0);
-}
-1-main.c
-#include "main.h"
-#include <stdio.h>
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
+if (c >= 48 && c <= 57)
 {
-char c;
-c = '0';
-printf("%c: %d\n", c, _isdigit(c));
-c = 'a';
-printf("%c: %d\n", c, _isdigit(c));
+return (1);
+}
 return (0);
 }
