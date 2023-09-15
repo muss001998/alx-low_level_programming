538b2c6fa9e45549f31741e66659ea012e440b3f
muss001998 <mustapha001hamiai@gmail.com>
2023-09-14 17:58:13 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -14,15 +14,3 @@ _putchar(c);
 }
 _putchar('\n');
 }
-4-main.c
-#include "main.h"
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-print_most_numbers();
-return (0);
-}
